5b80f8b6d4cc0b9a4da7d3881580128cb074a485
yous02 retro <musta.khalouk@gmail.com>
2021-10-08 15:23:55 +0200

    Generation token dynamique

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.SQLite;
+using EFTechCommerce.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using EFTechCommerce.Services;
 using WebApi.Entities;
 using WebApi.Models;
 
@@ -14,7 +14,6 @@ namespace WebApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-
         private readonly ILogger<AuthController> _logger;
 
         public AuthController(ILogger<AuthController> logger)
@@ -22,21 +21,22 @@ namespace WebApi.Controllers
             _logger = logger;
         }
 
-//         private string GenerateToken()
-//         {
-//             var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-// var stringChars = new char[8];
-// var random = new Random();
+        private static string GenerateToken()
+        {
+            var chars =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[64];
+            var random = new Random();
 
-// for (int i = 0; i < stringChars.Length; i++)
-// {
-//     stringChars[i] = chars[random.Next(chars.Length)];
-// }
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
 
-// var finalString = new String(stringChars);
-//         }
+            var finalString = new String(stringChars);
 
-    
+            return finalString;
+        }
 
         [HttpPost]
         [Route("login")]
@@ -46,18 +46,20 @@ namespace WebApi.Controllers
             User userInfo = usr.GetUser(user.Email);
             string userMail = userInfo.Email;
             string userPswd = userInfo.Password;
+
             // Console.WriteLine(userMail);
             // Console.WriteLine(user.Email);
             // Console.WriteLine(user.Password);
             // Console.WriteLine(userPswd);
-            
             string token = "";
-            if(userMail == user.Email && user.Password == userPswd) // Check mail & pswd
+            if (
+                userMail == user.Email && user.Password == userPswd // Check mail & pswd
+            )
             {
-                token = "test token";
-                usr.InsertToken(userMail, token);
+                token = GenerateToken();
+                usr.InsertToken (userMail, token);
             }
-            User returneduser = new User(userMail,  "", token);
+            User returneduser = new User(userMail, "", token);
             return returneduser;
         }
 
@@ -73,13 +75,11 @@ namespace WebApi.Controllers
         [Route("gettoken")]
         public User GetToken(string email)
         {
-
             UserDAO usr = new UserDAO();
             User userInfo = usr.GetUser(email);
 
             Console.WriteLine("this is : " + userInfo.Token);
             return userInfo;
         }
-        
     }
 }
